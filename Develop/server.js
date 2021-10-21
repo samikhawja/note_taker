2886ea5a3e52695e6ab7df1ff93d3b578ff216d2
samikhawja <skhawja11@gmail.com>
2021-10-20 22:30:11 -0700

    finshed serverJS

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./public/assets/js/index.js');
-const port = 3001;
+const PORT = 3001;
 const app = express();
 
 app.use(clog);
@@ -10,3 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () => 
+    console.log('App listening at http://localhost:${PORT} !')
+);
